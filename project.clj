cf46f379e8ddabcdc8b6fc222eebf1a9a3632944
mwkuster <marc@budabe.eu>
2012-10-13 15:38:09 +0200

    Project file description updated

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject uritemplate-clj "0.5.0-SNAPSHOT"
-  :description "Clojure implementation of URI Templates as specified in RFC 6570 (http://tools.ietf.org/html/rfc6570), currently level 3 compatible"
+(defproject uritemplate-clj "0.5.0"
+  :description "Clojure implementation of URI Templates as specified in RFC 6570 (http://tools.ietf.org/html/rfc6570), level 4 compliant"
   :url "https://github.com/mwkuster/uritemplate-clj"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :comment "Author: Marc Wilhelm Küster"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "4.0.3"]
                  [ring "1.1.6" :exclusions [org.clojure/clojure]]])
