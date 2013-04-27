7ea2d4366aa825c6b1b5c35de8a79f92c33b6186
mwkuster <marc@budabe.eu>
2013-04-27 11:37:55 +0200

    Test for ambiguous templates added, dependencies updated

---
+++ resources/configuration_template.clj
@@ -0,0 +1,8 @@
+{:min-confidence min-confidence, 
+ :chunk-size chunk-size,
+ :ngram-count ngram-count,
+ :source-file source-file,
+ :source-tags-filtered  '(:stage :castList :teiHeader :speaker :head :front),
+ :target-file target-file,
+ :target-file-filtered '(:stage :castList :teiHeader :speaker :front),
+ :visualization-framework "Neo4j"}
