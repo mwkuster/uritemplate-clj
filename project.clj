7093e35603a944f86f77f9faa19eae7434549c78
KUSTER Marc <marc.kuster@ec.europa.eu>
2019-08-02 10:57:41 +0200

    Clojure version updated to 1.10.1 (but continues to work with 1.9)

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject uritemplate-clj "1.2.2"
+(defproject uritemplate-clj "1.2.3"
   :description "Clojure implementation of URI Templates as specified in RFC 6570 (http://tools.ietf.org/html/rfc6570), level 4 compliant"
   :url "https://github.com/mwkuster/uritemplate-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :comment "Author: Marc Wilhelm Küster"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.8.1"]
                  [ring "1.7.1" :exclusions [org.clojure/clojure]]])
